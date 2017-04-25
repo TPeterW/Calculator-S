5d8d86beca9b0c77e0bcc5ba45a7a05e51077fb9
TPeterW <peterwangtao0@hotmail.com>
2017-04-24 22:23:35 -0400

    Changes

--- app/src/main/java/com/peter/calculator/MainActivity.java
+++ app/src/main/java/com/peter/calculator/MainActivity.java
@@ -2,14 +2,11 @@ package com.peter.calculator;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
